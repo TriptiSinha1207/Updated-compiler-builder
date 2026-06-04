eb2513d1142b9b2f066f769fc449d3ce1e13438f
Tripti Sinha <sinhatripti1207@gmail.com>
2026-06-04 15:36:38 +0530

    Add files via upload

--- build.sh
+++ build.sh
@@ -4,7 +4,5 @@ set -e
 echo "Installing dependencies from requirements.txt..."
 pip install -r requirements.txt
 
-echo "Installing local package 'pipeline'..."
-pip install -e .
-
 echo "Build complete!"
+
